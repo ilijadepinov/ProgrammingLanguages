9a3921de97afc9aaeee308b6a5758d219888ac30
ilijadepinov <147844848+ilijadepinov@users.noreply.github.com>
2023-10-20 18:44:38 +0200

    Create Decimals.java

---
+++ L4/Decimals.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class numberconversion {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter an integer: ");
+        int number = scanner.nextInt();
+        scanner.close();
+
+        System.out.println("Decimal: " + number);
+        System.out.println("Binary: " + Integer.toBinaryString(number));
+        System.out.println("Octal: " + Integer.toOctalString(number));
+        System.out.println("Hexadecimal: " + Integer.toHexString(number));
+    }
+}
